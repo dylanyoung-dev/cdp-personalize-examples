102adf3229868223faccb827962bc3f01be5520e
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2022-08-18 11:56:33 -0400

    CI Changes

--- artifacts/templates/decision/getNumberOfEvents/file.js
+++ artifacts/templates/decision/getNumberOfEvents/file.js
@@ -1,19 +1,19 @@
-// Use server-side JavaScript to create a programmable template 
+// Use server-side JavaScript to create a programmable template
 // You have full access to the guest context which can be accessed under guest, e.g. guest.email
 // This file is being controlled by Source Control!
 // This file will automatically get combined with your config.json and added to your tenant.
 
 (function () {
-  function getNumberOfEvents(session, eventType){
-      var numberOfEvents = 0;
-      for (var i = 0; i < session.events.length; i++) {
-          var event = session.events[i];
-          if (event.type === eventType) {
-              numberOfEvents++;
-          }
+  function getNumberOfEvents(session, eventType) {
+    var numberOfEvents = 0;
+    for (var i = 0; i < session.events.length; i++) {
+      var event = session.events[i];
+      if (event.type === eventType) {
+        numberOfEvents++;
       }
-      return numberOfEvents;
+    }
+    return numberOfEvents;
   }
 
-  var numberOfEvents =  getNumberOfEvents(triggerSession, "VIEW"); // triggerSession can be obtained using the getTriggerSession function
+  var numberOfEvents = getNumberOfEvents(triggerSession, 'VIEW'); // triggerSession can be obtained using the getTriggerSession function
 })();
