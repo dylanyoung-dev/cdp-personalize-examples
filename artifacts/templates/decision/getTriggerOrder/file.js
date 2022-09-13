1c64d02daad296a03fe19a5d4cca3ad2229ebef4
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2022-09-13 15:03:31 -0400

    ftl for freemarker

--- artifacts/templates/decision/getTriggerOrder/file.js
+++ artifacts/templates/decision/getTriggerOrder/file.js
@@ -2,21 +2,21 @@
 // This is for use in triggered experiences where the trigger is Order Created or Order Updated.
 
 (function () {
-    function getTriggerOrder(){
-        var orderRef;
-        if (entity && entity.ref !== 'undefined') {
-            orderRef = entity.ref;
-        }
-        var triggerOrder;
-        for (var i = 0; i < guest.orders.length; i++) {
-            var order = guest.orders[i];
-            if (order.ref === orderRef) {
-                triggerOrder = order;
-                break;
-            }
-        }
-        return triggerOrder;
+  function getTriggerOrder() {
+    var orderRef;
+    if (entity && entity.ref !== 'undefined') {
+      orderRef = entity.ref;
     }
+    var triggerOrder;
+    for (var i = 0; i < guest.orders.length; i++) {
+      var order = guest.orders[i];
+      if (order.ref === orderRef) {
+        triggerOrder = order;
+        break;
+      }
+    }
+    return triggerOrder;
+  }
 
-    var triggerOrder =  getTriggerOrder();
+  var triggerOrder = getTriggerOrder();
 })();
---
+++ artifacts/templates/web/tooltip/file.ftl
@@ -0,0 +1,5 @@
+<#-- Construct the API response using Freemarker -->
+<#-- For your Experience to run your API tab must have, at a minimum, open and closing brackets -->
+{
+    "just": "testing"
+}
